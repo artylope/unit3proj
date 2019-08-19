980a8d85c884056919d8c3a10954819973575593
kach92 <kenny.ang9206@gmail.com>
2019-08-19 20:07:14 +0800

    changed cart and order to receive furniture_option_id instead of furniture.id. adding to cart and order is working well now

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,12 +8,12 @@ class OrdersController < ApplicationController
         @carts = Cart.where(user_id: current_user.id).where(id: params[:order][:selected_cart_id])
         @order = Order.new
         @order.user_id = current_user.id
-        @order.total_price = @carts.map{|x|x.quantity*x.furniture.price}.sum
+        @order.total_price = @carts.map{|x|x.quantity*x.furniture_option.price}.sum
 
         @order.save
 
         @carts.each do |cart|
-            @furnitures_order = FurnituresOrder.new(order_id: @order.id, furniture_id: cart.furniture.id, quantity: cart.quantity)
+            @furnitures_order = FurnituresOrder.new(order_id: @order.id, furniture_option_id: cart.furniture_option.id, quantity: cart.quantity)
             @furnitures_order.save
         end
 
