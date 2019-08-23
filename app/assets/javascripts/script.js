328012b770d5528f27ea72f335312bd86e8b336f
Yi Xin Ang <angyixin@gmail.com>
2019-08-23 12:58:07 +0800

    change styles

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -116,7 +116,7 @@ const modalfunc = function(){
                         <td><img src="${x.image}" style="width:200px;"/</td>
                         <td>${x.quantity}</td>
                         <td>${(x.quantity*x.price).toFixed(2)}</td>
-                        <td><a class="button is-success cart-delete">Delete<input value="${x.cart_id}"hidden/></a></td>
+                        <td><a class="button is-danger cart-delete">Delete<input value="${x.cart_id}"hidden/></a></td>
                     </tr>
                     `)
 
@@ -263,6 +263,3 @@ $(document).ready(function(){
 
     Paloma.start();
 });
-
-
-
