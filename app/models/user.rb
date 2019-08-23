0206811d58a57adb0f36d54556301c9ca47250e8
kach92 <kenny.ang9206@gmail.com>
2019-08-23 15:55:18 +0800

    added validation for email

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
     devise :database_authenticatable, :registerable,
             :recoverable, :rememberable, :validatable
+    validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
     has_many :cart
 end
