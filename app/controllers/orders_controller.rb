fe65eaf5c366b58453ec802da952d1150906cd2c
kach92 <kenny.ang9206@gmail.com>
2019-08-24 14:37:54 +0800

    changed latest order at topmost

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,7 @@
 class OrdersController < ApplicationController
     before_action :authenticate_user!
     def index
-        @orders = Order.where(user_id: current_user.id)
+        @orders = Order.where(user_id: current_user.id).order(id: :desc)
         @carts = Cart.where(user_id: current_user.id)
         @wishlists = Wishlist.where(user_id: current_user.id)
     end
